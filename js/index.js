c7ff408631fdd3b691ba46a22cd2bef2a7921b1c
Zintra <114421435+Zintra@users.noreply.github.com>
2022-09-26 18:28:38 -0400

    Update index.js

--- js/index.js
+++ js/index.js
@@ -6,34 +6,34 @@ var alfaConcrete = [0.1, 0.05, 0.06, 0.07, 0.09, 0.08];
 var alfaPlaster = [0.013, 0.015, 0.02, 0.03, 0.04, 0.05];
 var alfaWood = [0.28, 0.22, 0.17, 0.09, 0.1, 0.11];
 var alfaCarpet = [0.02,	0.06,	0.14,	0.37,	0.6, 0.65];
-var alfaZintra 1/4" Sheets - Direct	= [0.00, 0.01, 0.06, 0.23, 0.52, 0.74];
-var alfaZintra 1/4" Sheets - Z Clips =	[0.05, 0.05, 0.13, 0.44, 0.81, 1.05];
-var alfaZintra 1/4" Sheets - 1" Air Gap =	[0.03, 0.02, 0.19, 0.46, 0.87, 0.96];
-var alfaZintra 1/4" Sheets - 2" Air Gap =	[0.00, 0.10, 0.33, 0.73, 0.97, 0.80];
-var alfaZintra 1/4" Sheets - 6" Air Gap =	[0.18, 0.48, 0.83, 0.81, 0.88, 0.89];
-var alfaZintra 1/2" Sheets - Direct =	[0.01, 0.05, 0.25, 0.61, 0.85, 0.95];
-var alfaZintra 1/2" Sheets - Z Clips =	[0.08, 0.13, 0.27, 0.72, 1.00, 1.06];
-var alfaZintra 1/2" Sheets - 1" Air Gap =	[0.04, 0.06, 0.38, 0.84, 1.00, 1.00];
-var alfaZintra 1/2" Sheets - 2" Air Gap =	[0.16, 0.46, 0.80, 1.04, 1.00, 0.92];
-var alfaZintra 1/2" Sheets - 6" Air Gap =	[0.46, 0.68, 0.96, 0.92, 0.94, 0.91];
-var alfaZintra 1" Sheets - Direct =	[0.04, 0.28, 0.78, 0.96, 1.01, 0.97];
-var alfaZintra 1" Sheets - Z Clips = [0.06, 0.28, 0.63, 1.02, 1.12, 1.08];
-var alfaZintra 1" Sheets - 1" Air Gap = [0.07, 0.37, 0.91, 1.00, 1.00, 1.00];
-var alfaZintra 1" Sheets - 2" Air Gap = [0.36, 0.77, 0.98, 1.00, 0.96, 0.99];
-var alfaZintra 1" Sheets - 6" Air Gap = [0.55, 0.69, 0.99, 0.86, 1.00, 1.00];
-var alfaZintra 2" Sheets - Direct = [0.18, 0.85, 1.15, 1.25, 1.18, 1.14];
-var alfaZintra 2" Sheets - Z Clips = [0.20, 0.87, 1.19, 1.31, 1.19, 1.14];
-var alfaZintra EMBOSS - Direct = [0.03, 0.03, 0.14, 0.40, 0.75, 0.88];
-var alfaZintra EMBOSS - 1" Air Gap = [0.00, 0.12, 0.33, 0.69, 0.97, 0.92];
-var alfaZintra EMBOSS - 2" Air Gap = [0.07, 0.31, 0.55, 0.84, 0.99, 0.82];
-var alfaZintra EMBOSS - 6" Air Gap = [0.27, 0.66, 0.85, 0.81, 0.92, 0.93];
-var alfaZintra Sticks Thin - Direct = [0.04, 0.25, 0.81, 1.17, 1.07, 1.14];
-var alfaZintra Sticks Thin - 6" Air Gap = [0.13, 0.31, 0.59, 1.05, 1.34, 1.48];
-var alfaZintra Blades Square Small 4.8" = [0.15, 0.51, 0.65, 0.71, 0.92, 1.12];
-var alfaZintra Blades Square 9.5" = [0.29, 0.69, 0.78, 1.08, 1.27, 1.40];
-var alfaZintra Baffles Small = [0.06, 0.21, 0.39, 0.67, 0.99, 1.44];
-var alfaZintra Baffles Large = [0.02, 0.52, 0.72, 1.17, 1.59, 1.95];
-var alfaZintra Box Tiles, Hex = [0.22, 0.81, 1.08, 1.30, 1.19, 1.18];
+var alfaZintraQuarterInchSheetsDirect	= [0.00, 0.01, 0.06, 0.23, 0.52, 0.74];
+var alfaZintraQuarterInchSheetsZClips =	[0.05, 0.05, 0.13, 0.44, 0.81, 1.05];
+var alfaZintraQuarterInchSheetsOneInchStandoff =	[0.03, 0.02, 0.19, 0.46, 0.87, 0.96];
+var alfaZintraQuarterInchSheetsTwoInchStandoff =	[0.00, 0.10, 0.33, 0.73, 0.97, 0.80];
+var alfaZintraQuarterInchSheetsSixInchStandoff =	[0.18, 0.48, 0.83, 0.81, 0.88, 0.89];
+var alfaZintraHalfInchSheetsDirect =	[0.01, 0.05, 0.25, 0.61, 0.85, 0.95];
+var alfaZintraHalfInchSheetsZClips =	[0.08, 0.13, 0.27, 0.72, 1.00, 1.06];
+var alfaZintraHalfInchSheetsOneInchStandoff =	[0.04, 0.06, 0.38, 0.84, 1.00, 1.00];
+var alfaZintraHalfInchSheetsTwoInchStandoff =	[0.16, 0.46, 0.80, 1.04, 1.00, 0.92];
+var alfaZintraHalfInchSheetsSixInchStandoff =	[0.46, 0.68, 0.96, 0.92, 0.94, 0.91];
+var alfaZintraOneInchSheetsDirect =	[0.04, 0.28, 0.78, 0.96, 1.01, 0.97];
+var alfaZintraOneInchSheetsZClips = [0.06, 0.28, 0.63, 1.02, 1.12, 1.08];
+var alfaZintraOneInchSheetsOneInchStandoff= [0.07, 0.37, 0.91, 1.00, 1.00, 1.00];
+var alfaZintraOneInchSheetsTwoInchStandoff = [0.36, 0.77, 0.98, 1.00, 0.96, 0.99];
+var alfaZintraOneInchSheetsSixInchStandoff = [0.55, 0.69, 0.99, 0.86, 1.00, 1.00];
+var alfaZintraTwoInchSheetsDirect = [0.18, 0.85, 1.15, 1.25, 1.18, 1.14];
+var alfaZintraTwoInchSheetsZClips = [0.20, 0.87, 1.19, 1.31, 1.19, 1.14];
+var alfaZintraEMBOSSDirect = [0.03, 0.03, 0.14, 0.40, 0.75, 0.88];
+var alfaZintraEMBOSSOneInchStandoff = [0.00, 0.12, 0.33, 0.69, 0.97, 0.92];
+var alfaZintraEMBOSSTwoInchStandoff= [0.07, 0.31, 0.55, 0.84, 0.99, 0.82];
+var alfaZintraEMBOSSSixInchStandoff = [0.27, 0.66, 0.85, 0.81, 0.92, 0.93];
+var alfaZintraSticksThinDirect = [0.04, 0.25, 0.81, 1.17, 1.07, 1.14];
+var alfaZintraSticksThinSixInchStandoff = [0.13, 0.31, 0.59, 1.05, 1.34, 1.48];
+var alfaZintraBladesSquareSmall = [0.15, 0.51, 0.65, 0.71, 0.92, 1.12];
+var alfaZintraBladesSquareLarge = [0.29, 0.69, 0.78, 1.08, 1.27, 1.40];
+var alfaZintraBafflesSmall = [0.06, 0.21, 0.39, 0.67, 0.99, 1.44];
+var alfaZintraBafflesLarge = [0.02, 0.52, 0.72, 1.17, 1.59, 1.95];
+var alfaZintraBoxTilesHex = [0.22, 0.81, 1.08, 1.30, 1.19, 1.18];
 
 
 var width = 1;
@@ -96,88 +96,88 @@ function getCoefficients(material) {
          return alfaCarpet;
          break;
       case 6:
-         return alfaZintra 1/4" Sheets - Direct;
+         return alfaZintraQuarterInchSheetsDirect;
          break;
       case 7:
-         return alfaZintra 1/4" Sheets - Z Clips;
+         return alfaZintraQuarterInchSheetsZClips;
          break;
       case 8:
-         return alfaZintra 1/4" Sheets - 1" Air Gap;
+         return alfaZintraQuarterInchSheetsOneInchStandoff;
          break;
       case 9:
-         return alfaZintra 1/4" Sheets - 2" Air Gap;
+         return alfaZintraQuarterInchSheetsTwoInchStandoff;
          break;
       case 10:
-         return alfaZintra 1/4" Sheets - 6" Air Gap;
+         return alfaZintraQuarterInchSheetsSixInchStandoff;
          break;     
       case 11:
-         return alfaZintra 1/2" Sheets - Direct;
+         return alfaZintraHalfInchSheetsDirect;
          break;  
       case 12:
-         return alfaZintra 1/2" Sheets - Z Clips;
+         return alfaZintraHalfInchSheetsZClips;
          break;  
       case 13:
-         return alfaZintra 1/2" Sheets - 1" Air Gap;
+         return alfaZintraHalfInchSheetsOneInchStandoff;
          break;  
       case 14:
-         return alfaZintra 1/2" Sheets - 2" Air Gap;
+         return alfaZintraHalfInchSheetsTwoInchStandoff;
          break;           
       case 15:
-         return alfaZintra 1/2" Sheets - 6" Air Gap;
+         return alfaZintraHalfInchSheetsSixInchStandoff;
          break;   
       case 16:
-         return alfaZintra 1" Sheets - Direct;
+         return alfaZintraOneInchSheetsDirect;
          break;   
       case 17:
-         return alfaZintra 1" Sheets - Z Clips;
+         return alfaZintraOneInchSheetsZClips;
          break;           
       case 18:
-         return alfaZintra 1" Sheets - 1" Air Gap;
+         return alfaZintraOneInchSheetsOneInchStandoff;
          break;      
       case 19:
-         return alfaZintra 1" Sheets - 2" Air Gap;
+         return alfaZintraOneInchSheetsTwoInchStandoff;
          break;           
       case 20:
-         return alfaZintra 1" Sheets - 6" Air Gap;
+         return alfaZintraOneInchSheetsSixInchStandoff;
          break;           
       case 21:
-         return alfaZintra 2" Sheets - Direct;
+         return alfaZintraTwoInchSheetsDirect;
          break;    
       case 22:
-         return alfaZintra 2" Sheets - Z Clips;
+         return alfaZintraTwoInchSheetsZClips;
          break;           
       case 23:
-         return alfaZintra EMBOSS - Direct;
+         return alfaZintraEMBOSSDirect;
          break;           
       case 24:
-         return alfaZintra EMBOSS - 1" Air Gap;
+         return alfaZintraEMBOSSOneInchStandoff;
          break;  
       case 25:
-         return alfaZintra EMBOSS - 2" Air Gap;
+         return alfaZintraEMBOSSTwoInchStandoff;
          break;           
       case 26:
-         return alfaZintra EMBOSS - 6" Air Gap;
+         return alfaZintraEMBOSSSixInchStandoff;
          break;           
       case 27:
-         return alfaZintra Sticks Thin - Direct;
+         return alfaZintraSticksThinDirect;
          break;  
       case 28:
-         return alfaZintra Sticks Thin - 6" Air Gap;
+         return alfaZintraSticksThinSixInchStandoff;
          break;           
       case 29:
-         return alfaZintra Blades Square Small 4.8" ;
+         return alfaZintraBladesSquareSmall;
          break;           
       case 30:
-         return alfaZintra Blades Square 9.5";
+         return alfaZintraBladesSquareLarge;
          break;      
       case 31:
-         return alfaZintra Baffles Small;
+         return alfaZintraBafflesSmall;
          break;           
       case 32:
-         return alfaZintra Baffles Large;
+         return alfaZintraBafflesLarge;
          break;         
       case 33:
-         return alfaZintra Box Tiles, Hex;
+         return ZintraBoxTilesHex;
          break;               
       default:
          break;
